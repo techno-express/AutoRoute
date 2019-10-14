89af6383c17c07ac9daee6f23e038046c7433119
techno-express <technoexpressnet@gmail.com>
2019-10-13 21:02:50 -0400

    bug fix, on Windows OS somehow an invalid '/' is being added to $this->directory and the whole path not being removed
    In reference to https://github.com/pmjones/AutoRoute/issues/6

--- src/Actions.php
+++ src/Actions.php
@@ -188,6 +188,13 @@ class Actions
     public function fileToClass(string $file) : ?string
     {
         $file = str_replace($this->directory . DIRECTORY_SEPARATOR, '', substr($file, 0, -4));
+
+        // Check for Windows OS like system, remove forward invalid '/', replace with '\'
+        if (('\\' === \DIRECTORY_SEPARATOR) && \strpos($file, ':\\') !== false) {
+            $this->directory = \str_replace('/', '\\', $this->directory);
+            $file = \str_replace($this->directory . \DIRECTORY_SEPARATOR, '', $file);
+        }
+
         $parts = explode(DIRECTORY_SEPARATOR, $file);
 
         $last = array_pop($parts);
